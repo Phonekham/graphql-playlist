45ad4060a56a26c7e362640563f07402d439d07e
Phonekham <phone.madridista@gmail.com>
2019-08-27 12:16:49 +0700

    The resolve function and test query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data
+var books = [
+  { name: "harry potter", genre: "wizarding world", id: "1" },
+  { name: "Merlin", genre: "wizarding world", id: "2" },
+  { name: "Hobbit", genre: "wizarding world", id: "3" }
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -19,6 +27,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
         // code to get data from db / other source
+        return _.find(books, { id: args.id });
       }
     }
   }
