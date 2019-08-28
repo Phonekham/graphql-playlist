790182cb304630ded8bc29ff583df363aca5fb6a
Phonekham <phone.madridista@gmail.com>
2019-08-28 10:06:23 +0700

    Connect DB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const mongoose = require("mongoose");
 const schema = require("./schema/schema");
 
 const app = express();
+
+// Connect DB
+mongoose.connect(
+  "mongodb+srv://phone:99491232@cluster0-dqv8d.mongodb.net/graphQLPlaylist?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
+);
+mongoose.connection.once("open", () => {
+  console.log("Connected Database");
+});
 
 app.use(
   "/graphql",
