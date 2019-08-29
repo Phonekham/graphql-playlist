8a1f2cfadc122c5cb0871cef16d9ef5ee6230105
Phonekham <phone.madridista@gmail.com>
2019-08-29 08:43:12 +0700

    BookList Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import BookList from "./components/BookList";
 
-function App() {
-  return (
-    <div id="main">
-      <h1>Reading List</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div id="main">
+        <h1>Reading List</h1>
+        <BookList></BookList>
+      </div>
+    );
+  }
 }
 
 export default App;
