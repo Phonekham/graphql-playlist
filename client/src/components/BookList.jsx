0a9522481463047f4e955cf396b6ed4e9a1e4b78
Phonekham <phone.madridista@gmail.com>
2019-08-29 10:14:35 +0700

    Rendering data in a Component

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -12,14 +12,22 @@ const getBooksQuery = gql`
 `;
 
 class BookList extends React.Component {
+  displayBooks() {
+    var data = this.props.data;
+    if (data.loading) {
+      return <div>Loading Books....</div>;
+    } else {
+      return data.books.map(book => {
+        return <li key={book.id}>{book.name}</li>;
+      });
+    }
+  }
   render() {
     console.log(this.props);
 
     return (
       <div>
-        <ul id="book-list">
-          <li>Book Name</li>
-        </ul>
+        <ul id="book-list">{this.displayBooks()}</ul>
       </div>
     );
   }
