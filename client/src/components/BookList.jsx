c02e7d634e3477b3471d3b8bb6d28846231216d7
Phonekham <phone.madridista@gmail.com>
2019-08-29 12:31:14 +0700

    Making a Single Query

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -5,13 +5,28 @@ import { getBooksQuery } from "../queries/queries";
 import BookDetails from "./bookDetails";
 
 class BookList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    };
+  }
   displayBooks() {
     var data = this.props.data;
     if (data.loading) {
       return <div>Loading Books....</div>;
     } else {
       return data.books.map(book => {
-        return <li key={book.id}>{book.name}</li>;
+        return (
+          <li
+            key={book.id}
+            onClick={e => {
+              this.setState({ selected: book.id });
+            }}
+          >
+            {book.name}
+          </li>
+        );
       });
     }
   }
@@ -21,7 +36,7 @@ class BookList extends React.Component {
     return (
       <div>
         <ul id="book-list">{this.displayBooks()}</ul>
-        <BookDetails></BookDetails>
+        <BookDetails bookId={this.state.selected}></BookDetails>
       </div>
     );
   }
